3d862acb9a42477f5c600d42d1fd1da4329280d4
BanNeigh <antoniobenny.jonathan@my.jcu.edu.au>
2021-04-09 19:32:39 +0700

    Add random number generator and refactor code to use functions

--- broken_score.py
+++ broken_score.py
@@ -2,13 +2,28 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if 90 <= score <= 100:
-    print("Excellent")
-elif 50 <= score < 90:
-    print("Passable")
-elif 0 <= score < 50:
-    print("Bad")
-else:
-    print("Invalid Score")
+
+def get_score():
+    score = float(input("Enter score: "))
+    return score
+
+
+def main(score):
+    if 90 <= score <= 100:
+        return "Excellent"
+    elif 50 <= score < 90:
+        return "Passable"
+    elif 0 <= score < 50:
+        return "Bad"
+    else:
+        return "Invalid Score"
+
+
+def get_random():
+    return random.randint(0, 100)
+
+
+print(main(get_score()))
+print(main(get_random()))
