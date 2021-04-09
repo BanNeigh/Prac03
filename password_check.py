2d6f75b873fcf600a19e83add826b064638c99ae
BanNeigh <antoniobenny.jonathan@my.jcu.edu.au>
2021-04-09 13:20:50 +0700

    Refactor program to use functions

--- password_check.py
+++ password_check.py
@@ -1,3 +1,15 @@
-passw = input("Enter password: ")
-for x in range(len(passw)):
-    print("*", end="")
+def main():
+    get_stars(get_password())
+
+
+def get_password():
+    passw = input("Enter password: ")
+    return passw
+
+
+def get_stars(passw):
+    for x in range(len(passw)):
+        print("*", end="")
+
+
+main()
