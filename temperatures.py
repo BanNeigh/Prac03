c2341b2cb7c82fd38a782456dac65447758b4863
BanNeigh <antoniobenny.jonathan@my.jcu.edu.au>
2021-04-09 13:40:44 +0700

    Add functions to code

--- temperatures.py
+++ temperatures.py
@@ -3,6 +3,15 @@ CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
 
+
+def celsius_to_fahr(celsius):
+    return celsius * 9.0 / 5 + 32
+
+
+def fahr_to_celsius(fahrenheit):
+    return 5 / 9 * (fahrenheit - 32)
+
+
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
@@ -11,12 +20,10 @@ choice = input(">>> ").upper()
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
+        print("Result: {:.2f} F".format(celsius_to_fahr(celsius)))
     elif choice == "F":
         fahrenheit = float(input("fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print("Result: {:.2f} C".format(celsius))
+        print("Result: {:.2f} C".format(fahr_to_celsius(fahrenheit)))
     else:
         print("Invalid option")
     print(MENU)
